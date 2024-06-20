aac238b64b567943629717b7ac46f4a1ba484db3
Imtiaz Ahmad <137213444+imtiazahmad-07@users.noreply.github.com>
2024-06-21 01:05:15 +0500

    Delete intto to js directory

--- intto to js/script.js
+++
@@ -1,9 +0,0 @@
-// Ways to print something in javascript
-console.log("Hello World")
-alert("No internet connection")
-document.write("Hello world");
-
-// Java script console API's
-console.log("Hello world");
-console.warn("Warning");
-console.error("This is an error ");
