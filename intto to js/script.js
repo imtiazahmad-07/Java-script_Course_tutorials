e13300f3b9e8bf2d04a46715a8bc972d2b6bec12
Imtiaz Ahmad <137213444+imtiazahmad-07@users.noreply.github.com>
2024-06-21 01:00:47 +0500

    Create script.js

---
+++ intto to js/script.js
@@ -0,0 +1,9 @@
+// Ways to print something in javascript
+console.log("Hello World")
+alert("No internet connection")
+document.write("Hello world");
+
+// Java script console API's
+console.log("Hello world");
+console.warn("Warning");
+console.error("This is an error ");
