9f7fe624d779bd8c9ee00131847db8588bbd46cd
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-06-21 01:10:09 +0500

    lec1 done

---
+++ lec1.js
@@ -0,0 +1,9 @@
+//Ways to print something in javascript
+console.log("Hello world");
+document.write("Hello world");
+alert("No internet connection");
+
+//2. Javascript console ApI's
+console.log("Hello");
+console.warn("Warning");
+console.error("THis is an error");
