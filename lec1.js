2c513acd39245dd02b984f221344f2d719ba5438
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-06-24 20:43:42 +0500

    learnt console api's

--- lec1.js
+++ lec1.js
@@ -1,9 +1,9 @@
 //Ways to print something in javascript
-console.log("Hello world");
+// console.log("Hello world");
 // document.write("Hello world");
 // alert("No internet connection");
 
 //2. Javascript console ApI's
-console.log("Hello");
+console.log("Hello World");
 console.warn("Warning");
 console.error("This is an error");
