37cd19d7013e4b411f12d7bd9f525b2e622307fb
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-06-24 21:34:56 +0500

    learnt Strings

---
+++ lec7.js
@@ -0,0 +1,9 @@
+//===========================  Intro to Strings    =====================
+let name="Imtiaz";
+console.log("Name is : ",name);
+console.log("Length of name is :",name.length );
+console.log("First letter of ",name," is ",name[0]);
+
+//Template literal
+sentence=`${name} is a student`;
+console.log(sentence);
