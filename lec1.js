a9f7fc354b8785e06a43475eaf99bd0fb99e6fed
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-06-24 20:37:29 +0500

    modified the repo

--- lec1.js
+++ lec1.js
@@ -1,9 +1,9 @@
 //Ways to print something in javascript
 console.log("Hello world");
-document.write("Hello world");
-alert("No internet connection");
+// document.write("Hello world");
+// alert("No internet connection");
 
 //2. Javascript console ApI's
 console.log("Hello");
 console.warn("Warning");
-console.error("THis is an error");
+console.error("This is an error");
---
+++ lec5.js
@@ -0,0 +1,19 @@
+let hour=12;
+if(hour >= 6 && hour <12)
+    {
+        console.log("Good morning");
+    }
+else if(hour >= 12 && hour < 18)
+    {
+        console.log("Good Afternoon");
+    }
+else if (hour >=18 && hour < 20){
+    console.log ("Good Evening");
+}
+else if (hour >=20 && hour < 24 || hour >=1 && hour <6)
+    {
+        console.log("Good Night");
+    }
+else{
+    console.log("Invalid")
+}
