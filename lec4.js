f0a7b1220e0e8046a10aa22218af25d6b6419921
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-06-24 19:52:29 +0500

    practiced more

---
+++ lec4.js
@@ -0,0 +1,17 @@
+var a=4,b=2;
+sum=a+b;
+subtraction=a-b;
+product=a*b;
+quotient=a/b;
+remainder=a%b;
+exponential=a**b;
+
+document.write("First number is ",a);
+document.write("<br>Second number is : ",b,"<br>");
+
+document.write("<br>Sum is : ",sum);
+document.write("<br>Difference is : ",subtraction);
+document.write("<br>Product is : ",product);
+document.write("<br>Quotient is : ",quotient);
+document.write("<br>Remainder is : ",remainder);
+document.write("<br>Exponential is : ",exponential);
