8bba6498a120727d00647d6fb5b0c57db40e0ae5
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-06-25 20:44:01 +0500

    for of loop done

---
+++ lec13.js
@@ -0,0 +1,12 @@
+// For-of-loop
+
+let name = ["imtiaz","waseem","sohail"];            //an array
+let alphabets="abcdefjhijklmnopqrstuvwxyz"          //A string
+for (let i of name )                    //getting names forom name array
+    {   
+        console.log(i);
+    }
+for(let j of alphabets)                 //getting alphabets from alphabets string
+    {
+        console.log(j);
+    } 
