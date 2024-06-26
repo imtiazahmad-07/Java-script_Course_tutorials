7c2213bb1724e73abc57677a1cd4ff78d362dba5
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-06-26 14:03:35 +0500

    Arrow function

---
+++ lec17.js
@@ -0,0 +1,13 @@
+//arrow function of sum function    
+const sum = (a,b) => {
+    return a+b ;
+}
+add=sum(4,2);
+console.log("Sum is ",add);
+
+//Arrow function of Multiplication function
+const mul = (a,b)=>{
+    return a*b ;
+}
+product= mul(4,2);
+console.log("Product is : ",product);
