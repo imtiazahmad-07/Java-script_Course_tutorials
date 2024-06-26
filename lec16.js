068358830f541b357b3d4c097dc2b898e96fa49f
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-06-26 13:45:15 +0500

    function lecture

---
+++ lec16.js
@@ -0,0 +1,18 @@
+// //Funtion printing a message only
+
+// function message(){
+//     console.log("Hello World");
+// }
+
+// message(); //calling the function
+
+
+//Function retruning and passing the value
+
+function sum(a,b){
+    let add = a+b;
+    return add;
+}
+let num1 =10, num2 =20;
+let result = sum(num1, num2);
+console.log("Sum is : ",result)
