8b5b4a666c28b8358f51cda0e4d2f07df1c0ab38
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-07-18 21:44:29 +0500

    Async await

---
+++ lec29.js
@@ -0,0 +1,20 @@
+function getData(dataId)
+{
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            console.log("Data",dataId)
+            resolve("success")
+        }, 2000);
+    })
+}
+
+async function callData(){
+    console.log("Getting data 1....")
+    await getData(1);
+    console.log("Getting data 2....")
+    await getData(2);
+    console.log("Getting data 3....")
+    await getData(3);
+    console.log("Getting data 4....")
+    await getData(4);
+}
