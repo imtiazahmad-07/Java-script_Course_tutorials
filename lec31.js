2bff434c2b7344e0634e8be05823b7e996b68cec
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-07-21 21:34:50 +0500

    Get methods

--- lec31.js
+++ lec31.js
@@ -1,2 +1,16 @@
 /////////////// DOM concepts    //////////////////////////////
 
+var element,element1,element2,element3,nextElement;
+element = document.getElementById("google-link").innerText;
+nextElement = document.getElementById("para1").innerHTML;
+element1 = document.getElementById("text").getAttribute("height");
+element2 = document.getElementById("text").getAttributeNode("style");
+element3 = document.getElementById("text").attributes[2].value;
+
+
+console.log(element);
+console.log(element1);
+console.log(element2);
+console.log(element3)
+
+console.log(nextElement);
