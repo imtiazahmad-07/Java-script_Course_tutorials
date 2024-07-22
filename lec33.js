991ce3ac12f7ae71384d6528cfb7ff722d4d23b1
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-07-22 20:20:17 +0500

    DOM properties

---
+++ lec33.js
@@ -0,0 +1,14 @@
+// DOM properties
+
+let firstElement = document.querySelector("body").children; //gives the child nodes of body tag
+
+let text = document.querySelector("div").innerText;  //gives the text only
+
+let html = document.querySelector("body").innerHTML; //gives the content html syntax both
+
+let hiddenText = document.querySelector("h4").textContent;  //it shows the hidden text on console screen
+
+console.log(firstElement)
+console.log(text)
+console.log(html)
+console.log(hiddenText)
