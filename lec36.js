1e300bac56c7e2ae17ffe6b235611fe19135976d
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-07-25 19:18:19 +0500

    Mouse Events

--- lec36.js
+++ lec36.js
@@ -1,3 +1,19 @@
-let fun = ()=>{
-    alert("Event Activated")
-}
+let box1 = document.querySelector("#box1");
+let box2 = document.querySelector("#box2");
+let box3 = document.querySelector("#box3");
+
+box1.addEventListener("click",()=>{
+    alert("Box1 Clicked");
+})  //It occurs on left click
+
+box2.addEventListener("contextmenu",()=>{
+    alert("Box2 Clicked")
+}) ////It occurs on right click
+
+box3.addEventListener("dblclick",()=>{
+    alert("Box3 Clicked")
+}) ////It occurs on double click
+
+box4.addEventListener("mouseover",()=>{
+    alert("Box4 Hovered")
+}) ////It occurs on when the pointer is over the box
